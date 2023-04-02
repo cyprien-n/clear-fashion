2a5add64e7eac4d0bc18ed68d719d4ef4a700c30
cyprien-n <cyprien.nicolay@gmail.com>
2023-04-03 00:22:15 +0200

    update

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -24,8 +24,23 @@ let currentPagination = {};
 // instantiate the selectors
 const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
+const selectBrand = document.querySelector('#brand-select');
+const selectSort = document.querySelector('#sort-select');
+const selectReasonablyPriced = document.querySelector('#reasonably-select');
+const selectRecentlyReleased = document.querySelector('#recently-select');
+const selectByFavorite = document.querySelector('#favorite-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
+
+
+// indicators
+
+let recentProducts = 0
+let lastReleaseDate = NaN;
+let p50 = 0;
+let p90 = 0;
+let p95 = 0;
+
 
 /**
  * Set global value
@@ -43,10 +58,10 @@ const setCurrentProducts = ({result, meta}) => {
  * @param  {Number}  [size=12] - size of the page
  * @return {Object}
  */
-const fetchProducts = async (page = 1, size = 12) => {
+const fetchProducts = async (page = 1, size = 12,brand='all',reasonablyPriced=false,recentlyReleased=false,sort='price-asc',favoriteProds=false) => {
   try {
     const response = await fetch(
-      `https://clear-fashion-api.vercel.app?page=${page}&size=${size}`
+      `https://clear-fashion-api.vercel.app?&size=999`+ (brand !== "all" ? `&brand=${brand}` : "")
     );
     const body = await response.json();
 
@@ -54,13 +69,100 @@ const fetchProducts = async (page = 1, size = 12) => {
       console.error(body);
       return {currentProducts, currentPagination};
     }
+    
+    var result = body.data.result;
 
-    return body.data;
+    if(reasonablyPriced){
+      result = result.filter(product => product.price<50)
+    }
+
+    if(recentlyReleased){
+      result = result.filter(product => (new Date() - new Date(product.released)) / (1000 * 60 * 60 * 24) < 50);
+    }
+
+    var favorites = JSON.parse(localStorage.getItem('favorites'));
+
+    if(favoriteProds){
+      result = result.filter(product => favorites.includes(product.uuid) );
+    }
+
+    if(sort === "price-asc") {
+      result.sort((a, b) => a.price - b.price);
+    }
+    else if(sort === "price-desc") {
+      result.sort((a, b) => b.price - a.price);
+    }
+    else if(sort === "date-asc") {
+      result.sort((a, b) => new Date(b.released) - new Date(a.released));
+    }
+    else if(sort === "date-desc") {
+      result.sort((a, b) => new Date(a.released) - new Date(b.released));
+    }
+    
+    // indicators
+    
+    recentProducts = result.filter(product => (new Date() - new Date(product.released)) / (1000 * 60 * 60 * 24) < 50).length;
+    lastReleaseDate = result.sort((a, b) => new Date(b.released) - new Date(a.released))[0].released;
+
+    if(result.length > 0)
+    {
+      p50 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length / 2)].price;
+      p90 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length * 0.9)].price;
+      p95 = [...result].sort((a, b) => a.price - b.price)[Math.floor(result.length * 0.95)].price;
+    }
+    else
+    {
+      p50 = 0;
+      p90 = 0;
+      p95 = 0;
+    }
+
+    var meta = {
+      currentPage: page,
+      pageCount: Math.ceil(result.length / size),
+      pageSize: size,
+      count: result.length
+    };
+
+    var result = result.slice((page - 1) * size, page * size);
+    
+    return {result,meta};
+
   } catch (error) {
     console.error(error);
     return {currentProducts, currentPagination};
   }
 };
+
+/**
+ * fetch brand 
+ * @return {Array} brands
+ */
+
+const fetchBrands = async () => {
+  try {
+    const response = await fetch(
+      'https://clear-fashion-api.vercel.app/brands'
+      );
+
+    const body = await response.json();
+
+    if (body.success !== true) {
+      console.error(body);
+    }
+    else{
+      var brands = body.data.result;
+      brands.unshift('all')
+      renderBrands(brands)
+    }
+
+    return body.data.result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
 
 /**
  * Render list of products
@@ -75,11 +177,12 @@ const renderProducts = products => {
       <div class="product" id=${product.uuid}>
         <span>${product.brand}</span>
         <a href="${product.link}">${product.name}</a>
-        <span>${product.price}</span>
+        <span>${product.price} €</span>
+        <span id="${product.uuid}-fav">` + ((JSON.parse(localStorage.getItem("favorites"))||[]).includes(product.uuid)?`<button onclick=deleteFromFavorites("` + product.uuid + `")>❤️</button>`:`<button onclick=addToFavorites("` + product.uuid + `")>🤍</button>`)
+        +`</span>
       </div>
-    `;
-    })
-    .join('');
+    `})
+.join('');
 
   div.innerHTML = template;
   fragment.appendChild(div);
@@ -102,15 +205,39 @@ const renderPagination = pagination => {
   selectPage.selectedIndex = currentPage - 1;
 };
 
+
+
 /**
  * Render page selector
  * @param  {Object} pagination
  */
 const renderIndicators = pagination => {
   const {count} = pagination;
-
   spanNbProducts.innerHTML = count;
+  const spanNbRecentProducts = document.querySelector('#nbRecentProducts');
+  spanNbRecentProducts.innerHTML = recentProducts;
+  const spanp50 = document.querySelector('#p50');
+  spanp50.innerHTML = p50;
+  const spanp90 = document.querySelector('#p90');
+  spanp90.innerHTML = p90;
+  const spanp95 = document.querySelector('#p95');
+  spanp95.innerHTML = p95;
+  const spanLastReleasedDate = document.querySelector('#lastReleasedDate');
+  spanLastReleasedDate.innerHTML = lastReleaseDate;
 };
+
+
+
+
+/**
+ * Render brand selector
+ * @param  {Array} brands
+ */
+const renderBrands = brands => {
+  const options = brands.map(brand => `<option value="${brand}">${brand}</option>`).join('')
+  selectBrand.innerHTML = options;
+};
+
 
 const render = (products, pagination) => {
   renderProducts(products);
@@ -126,15 +253,93 @@ const render = (products, pagination) => {
  * Select the number of products to display
  */
 selectShow.addEventListener('change', async (event) => {
-  const products = await fetchProducts(currentPagination.currentPage, parseInt(event.target.value));
+  const products = await fetchProducts(currentPagination.currentPage, parseInt(event.target.value),selectBrand.value,selectReasonablyPriced.checked,selectRecentlyReleased.checked,selectSort.value,selectByFavorite.checked);
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
+
+selectPage.addEventListener('change', async (event) => {
+
+  const numberOfPage = parseInt(event.target.value);
+  const products = await fetchProducts(numberOfPage,currentPagination.pageSize,selectBrand.value,selectReasonablyPriced.checked,selectRecentlyReleased.checked,selectSort.value,selectByFavorite.checked);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+selectBrand.addEventListener('change', async (event) => {
+  const products = await fetchProducts(currentPagination.currentPage,currentPagination.pageSize,event.target.value,selectReasonablyPriced.checked,selectRecentlyReleased.checked,selectSort.value,selectByFavorite.checked);
+
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+});
+
+
+if(selectReasonablyPriced){
+selectReasonablyPriced.addEventListener('change',async(event)=>{
+  const products = await fetchProducts(currentPagination.currentPage,currentPagination.pageSize,selectBrand.value,event.target.checked,selectRecentlyReleased.checked,selectSort.value,selectByFavorite.checked);
+  setCurrentProducts(products);
+  render(currentProducts, currentPagination);
+})
+}
+
+if(selectRecentlyReleased){
+  selectRecentlyReleased.addEventListener('change',async(event)=>{
+    const products = await fetchProducts(currentPagination.currentPage,currentPagination.pageSize,selectBrand.value,selectReasonablyPriced.checked,event.target.checked,selectSort.value,selectByFavorite.checked);
+    setCurrentProducts(products);
+    render(currentProducts, currentPagination);
+  })
+  }
+
+  if(selectByFavorite){
+    selectByFavorite.addEventListener('change',async(event)=>{
+      const products = await fetchProducts(currentPagination.currentPage,currentPagination.pageSize,selectBrand.value,selectReasonablyPriced,selectRecentlyReleased.checked,selectSort.value,event.target.checked);
+      setCurrentProducts(products);
+      render(currentProducts, currentPagination);
+    })
+    }
+
+  selectSort.addEventListener('change', async (event) => {
+    const products = await fetchProducts(currentPagination.currentPage,currentPagination.pageSize,selectBrand.value,selectReasonablyPriced.checked,selectRecentlyReleased.checked,event.target.value,selectByFavorite.checked);
+  
+    setCurrentProducts(products);
+    render(currentProducts, currentPagination);
+  });
+  
+
 
 document.addEventListener('DOMContentLoaded', async () => {
   const products = await fetchProducts();
-
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
+
+
+fetchBrands();
+
+
+// Add item to favorites
+function addToFavorites(id) {
+  // Get existing favorites or create empty array
+  var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  // Add item to favorites array if not already present
+  if (!favorites.includes(id)) {
+    favorites.push(id);
+    console.log(favorites)
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+    document.getElementById(id + "-fav").innerHTML = ` <button onclick=deleteFromFavorites("` + id + `")>❤️</button>`;
+  }
+}
+
+// delete item from favorites
+function deleteFromFavorites(id) {
+  // Get existing favorites or create empty array
+  var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  // delete item from favorites
+  favorites = favorites.filter(favorite => favorite != id);
+  console.log(favorites)
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+  document.getElementById(id + "-fav").innerHTML = ` <button onclick=addToFavorites("` + id + `")>🤍</button>`;
+
+}
